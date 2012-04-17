c0b00793421b910261d936749535c439a65052ff
Stefano Rivera <stefano.rivera@yola.com>
2012-04-17 18:14:09 +0200

    Enable /accounts/login, which djangopypi will redirect to, if necessary

--- src/yolapi/settings.py
+++ src/yolapi/settings.py
@@ -117,6 +117,7 @@ TEMPLATE_DIRS = (
     # "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    dconf.install_path + '/templates',
 )
 
 INSTALLED_APPS = (
--- src/yolapi/urls.py
+++ src/yolapi/urls.py
@@ -4,6 +4,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include("djangopypi.urls")),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
