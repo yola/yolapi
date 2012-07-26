43ec06be66afd32108d5160b61898a409e5026f8
Stefano Rivera <stefano.rivera@yola.com>
2012-07-26 16:47:45 +0200

    Use config.common.domain.services

--- deploy/configuration/yolapi-default.py
+++ deploy/configuration/yolapi-default.py
@@ -13,7 +13,7 @@ def update(config):
                 'data': data_path,
                 'log': '/var/log/yolapi.log',
             },
-            'domain': 'yolapi.yola.net',
+            'domain': 'yolapi.%s' % config.common.domain.services,
             'db':  {
                 'name': os.path.join(data_path, 'yolapi.sqlite'),
                 'engine': 'django.db.backends.sqlite3',
