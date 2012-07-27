886d341084ab044e92da47d598b188560ab016ac
Stefano Rivera <stefano.rivera@yola.com>
2012-07-27 16:54:14 +0200

    Add robots.txt

--- src/yolapi/urls.py
+++ src/yolapi/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
@@ -6,6 +7,8 @@ urlpatterns = patterns('',
     url(r'', include("djangopypi.urls")),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
         {'template_name': 'login.html'}),
+    url(r'^robots.txt$', direct_to_template,
+        {'template': 'robots.txt', 'mimetype': 'text/plain'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
