802f2db86aa0a3dad5fc619d405c3524d3851b41
Stefano Rivera <stefano.rivera@yola.com>
2012-07-27 14:53:36 +0200

    And that required some settings.py changes

--- yolapi/settings.py
+++ yolapi/settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'yolapi.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'yolapi.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or
