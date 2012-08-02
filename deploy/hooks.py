bfc25040d40805e0dc37df1ffaf05cd62f89cdc0
Stefano Rivera <stefano.rivera@yola.com>
2012-08-02 16:37:51 +0200

    Use has_media

--- deploy/hooks.py
+++ deploy/hooks.py
@@ -4,6 +4,7 @@ from yola.deploy.hooks.django import DjangoApp
 class Hooks(DjangoApp):
     migrate_on_deploy = True
     uses_south = True
+    has_media = True
 
 
 hooks = Hooks
--- yolapi/settings.py
+++ yolapi/settings.py
@@ -52,7 +52,7 @@ USE_L10N = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = aconf.path.data + '/'
+MEDIA_ROOT = os.path.join(aconf.path.data, 'media') + '/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
