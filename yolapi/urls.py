5d7ad7770ce6799d31e57e7677000c0f8f219475
Stefano Rivera <stefano.rivera@yola.com>
2012-08-03 13:42:12 +0200

    Use Only serve /media/ from Django when DEBUG is on

--- yolapi/urls.py
+++ yolapi/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
-from django.conf.urls.static import static
 from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 
@@ -15,4 +14,10 @@ urlpatterns = patterns('',
         {'template': 'robots.txt', 'mimetype': 'text/plain'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    )
