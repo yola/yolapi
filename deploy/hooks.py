b7404967ded450e7d0a827a9e5869b2ced003eb7
Stefano Rivera <stefano.rivera@yola.com>
2012-10-03 17:34:14 +0200

    apache needs to enforce http auth now

--- deploy/hooks.py
+++ deploy/hooks.py
@@ -1,10 +1,12 @@
 from yola.deploy.hooks.django import DjangoApp
+from yola.deploy.hooks.htpasswd import AuthenticatedApp
 
 
-class Hooks(DjangoApp):
+class Hooks(DjangoApp, AuthenticatedApp):
     migrate_on_deploy = True
     uses_south = True
     has_media = True
+    has_static = True
 
 
 hooks = Hooks
