385e4929a93a4f88e164c9df55c0aa15cbb8a6aa
Stefano Rivera <stefano.rivera@yola.com>
2012-10-16 15:52:50 +0200

    Add management command to list & delete orphans

---
+++ yolapi/pypi/management/commands/orphans.py
@@ -0,0 +1,33 @@
+import os
+from optparse import make_option
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from yolapi.pypi.models import Distribution
+
+
+class Command(BaseCommand):
+    help = 'List orphaned distributions on the filesystem'
+    option_list = BaseCommand.option_list + (
+        make_option('--delete',
+                    action='store_true',
+                    dest='delete',
+                    default=False,
+                    help='Delete orphaned files instead of simply listing'),
+    )
+
+    def handle(self, *args, **options):
+        directory = os.path.join(settings.MEDIA_ROOT,
+                                 getattr(settings, 'PYPI_DISTS', 'dists'))
+        on_disk = set(os.listdir(directory))
+        owned = set()
+        for distribution in Distribution.objects.iterator():
+            owned.add(distribution.filename)
+
+        orphans = on_disk.difference(owned)
+
+        for filename in sorted(orphans):
+            print filename
+            if options['delete']:
+                os.unlink(os.path.join(directory, filename))
