b879c5bf7e23269ff3f24cba2c87cd710012f928
Stefano Rivera <stefano@yola.com>
2013-05-16 14:25:16 +0200

    MissingValue doesn't need an argument any more

--- deploy/configuration/yolapi-default.py
+++ deploy/configuration/yolapi-default.py
@@ -10,8 +10,8 @@ def update(config):
             'allowed_uploaders': ['yola'],
             'build_eggs_for': ['2.6'],
             'aws': {
-                'access_key': MissingValue('yolapi.aws.access_key'),
-                'secret_key': MissingValue('yolapi.aws.secret_key'),
+                'access_key': MissingValue(),
+                'secret_key': MissingValue(),
                 'archive_bucket': 'yolapi.%s' % config.common.domain.services,
             },
             'debug': False,
@@ -22,8 +22,8 @@ def update(config):
                 'celery_log': '/var/log/yolapi-worker.log',
             },
             'ssl': {
-                'cert': MissingValue('yolapi.ssl.cert'),
-                'key': MissingValue('yolapi.ssl.key'),
+                'cert': MissingValue(),
+                'key': MissingValue(),
             },
             'domain': 'yolapi.%s' % config.common.domain.services,
             'db':  {
