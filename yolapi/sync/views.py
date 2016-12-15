84ef2b05111134eba61a75c267c65e519f5b5f52
Thomas Welfley <thomas.welfley@gmail.com>
2016-12-15 12:02:11 -0800

    sync.views: Correct import conflict

--- yolapi/sync/views.py
+++ yolapi/sync/views.py
@@ -1,10 +1,10 @@
-from django.views.decorators.http import require_POST
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
 
-import sync.tasks
+from sync import tasks
 
 
 @require_POST
 def sync(request):
-    sync.tasks.sync.delay()
+    tasks.sync.delay()
     return HttpResponse('Sync Queued', content_type='text/plain')
