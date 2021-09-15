4891a598c4ae5655e7867b52d550283b0b0e8e1e
kosmos342 <kosmos342@gmail.com>
2021-09-15 13:09:55 +0300

    boto->boto3 for s3 sync

--- yolapi/sync/tasks.py
+++ yolapi/sync/tasks.py
@@ -1,9 +1,9 @@
-import base64
 import logging
 import urllib
 
-import boto.s3.connection
+import boto3
 import dateutil.parser
+from botocore.exceptions import ClientError
 from django.conf import settings
 from django.core.files import File
 
@@ -22,9 +22,9 @@ def sync():
     bucket = _bucket()
 
     s3_distributions = {}
-    for key in bucket.list(delimiter='/', prefix='dists/'):
-        if isinstance(key, boto.s3.key.Key):
-            s3_distributions[key.name.split(u'/', 1)[1]] = key
+    for s3_obj_info in bucket.objects.filter(Delimiter='/', Prefix='dists/'):
+        s3_obj = s3_obj_info.Object()
+        s3_distributions[s3_obj.key.split(u'/', 1)[1]] = s3_obj
 
     # Our Distribution table doesn't store the bare filename, but we're about
     # to do a full table scan, so no loss...
@@ -38,19 +38,19 @@ def sync():
             push.delay(distribution.id)
             continue
 
-        key = s3_distributions[distribution.filename]
-        if allow_replacement and _compare_db_with_s3(distribution, key) > 0:
+        s3_obj = s3_distributions[distribution.filename]
+        if allow_replacement and _compare_db_with_s3(distribution, s3_obj) > 0:
             log.info(u"Queueing push: %s [mismatch]", distribution.filename)
             push.delay(distribution.id)
 
-    for filename, key in s3_distributions.iteritems():
+    for filename, s3_obj in s3_distributions.iteritems():
         if filename not in by_filename:
             log.info(u"Queueing pull: %s [missing]", filename)
             pull.delay(filename)
             continue
 
         distribution = Distribution.objects.get(id=by_filename[filename])
-        if allow_replacement and _compare_db_with_s3(distribution, key) < 0:
+        if allow_replacement and _compare_db_with_s3(distribution, s3_obj) < 0:
             log.info(u"Queueing pull %s [mismatch]", filename)
             pull.delay(filename)
 
@@ -63,36 +63,45 @@ def push(id):
     log.info(u"Pushing %s", distribution.filename)
 
     bucket = _bucket()
-    key = bucket.get_key(u'dists/%s' % distribution.filename)
-    if key is not None:
+    s3_obj = bucket.Object(u'dists/%s' % distribution.filename)
+    try:
+        s3_obj.load()
+    except ClientError as e:
+        if e.response['Error']['Code'] != '404':
+            raise
+    else:
         if not allow_replacement:
             log.warn("Aborting replacement")
             return
-        if _compare_db_with_s3(distribution, key) <= 0:
+        if _compare_db_with_s3(distribution, s3_obj) <= 0:
             log.warn("Aborting push on top of a newer object")
             return
 
-    key = bucket.new_key(u'dists/%s' % distribution.filename)
-
-    key.set_metadata('md5_digest', distribution.md5_digest)
-    key.set_metadata('package', release.package.name)
-    key.set_metadata('version', release.version)
-    key.set_metadata('filetype', distribution.filetype)
-    uploaded = urllib.quote(distribution.created.isoformat())
-    key.set_metadata('uploaded', uploaded)
+    metadata = {
+        'md5_digest': distribution.md5_digest,
+        'package': release.package.name,
+        'version': release.version,
+        'filetype': distribution.filetype,
+        'uploaded': urllib.quote(distribution.created.isoformat())
+    }
     if distribution.pyversion:
-        key.set_metadata('pyversion', distribution.pyversion)
+        metadata['pyversion'] = distribution.pyversion
 
-    key.set_contents_from_filename(filename=distribution.content.path,
-                                   md5=_boto_md5sum(distribution.md5_digest),
-                                   replace=True)
+    s3_obj.put(
+        Body=distribution.content,
+        ContentMD5=_boto3_md5sum(distribution.md5_digest),
+        Metadata=metadata,
+    )
 
-    key = bucket.new_key(u'releases/%s/%s' %
-                         (release.package.name,
-                          release.version))
-    key.set_metadata('package', release.package.name)
-    key.set_metadata('version', release.version)
-    key.set_contents_from_string(release.metadata, replace=True)
+    bucket.Object(
+        u'releases/{}/{}'.format(release.package.name, release.version)
+    ).put(
+        Body=release.metadata,
+        Metadata={
+            'package': release.package.name,
+            'version': release.version,
+        }
+    )
 
 
 @local_celery_app.task
@@ -101,17 +110,22 @@ def pull(filename):
     log.info(u"Pulling %s", filename)
 
     bucket = _bucket()
-    key = bucket.get_key(u'dists/%s' % filename)
-    if key is None:
-        log.warn('Aborting pull of non-existent file')
-        return
+    s3_obj = bucket.Object(u'dists/%s' % filename)
+    try:
+        s3_obj.load()
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            log.warn('Aborting pull of non-existent file')
+            return
+        raise
 
-    package = key.get_metadata('package')
-    version = key.get_metadata('version')
-    filetype = key.get_metadata('filetype')
-    pyversion = key.get_metadata('pyversion') or u''
-    md5_digest = key.get_metadata('md5_digest')
-    uploaded = dateutil.parser.parse(key.get_metadata('uploaded'))
+    package = s3_obj.metadata['package']
+    version = s3_obj.metadata['version']
+    filetype = s3_obj.metadata['filetype']
+    pyversion = s3_obj.metadata.get('pyversion', u'')
+    md5_digest = s3_obj.metadata['md5_digest']
+    uploaded = dateutil.parser.parse(
+        urllib.unquote(s3_obj.metadata['uploaded']))
 
     package, _ = Package.objects.get_or_create(name=package)
     release, created = package.releases.get_or_create(version=version)
@@ -122,7 +136,7 @@ def pull(filename):
         if not allow_replacement:
             log.warn("Aborting replacement")
             return
-        if _compare_db_with_s3(distribution, key) >= 0:
+        if _compare_db_with_s3(distribution, s3_obj) >= 0:
             log.warn("Aborting pull on top of a newer object")
             return
         distribution.delete()
@@ -134,40 +148,36 @@ def pull(filename):
         filetype=filetype,
         pyversion=pyversion,
         md5_digest=md5_digest,
-        content=File(key, name=filename),
+        content=File(s3_obj.get()['Body'], name=filename),
         created=uploaded,
         sync_imported=True)
     distribution.save()
 
-    key = bucket.get_key(u'releases/%s/%s' %
-                         (release.package.name,
-                          release.version))
-    release.metadata = key.get_contents_as_string()
+    s3_obj = bucket.Object(u'releases/%s/%s' %
+                           (release.package.name,
+                            release.version))
+    release.metadata = s3_obj.get()['Body'].read().decode('utf-8')
     release.save()
 
 
-def _boto_md5sum(digest):
-    """Return the (digest, b64) pair tat boto expects"""
-    return (digest,
-            base64.encodestring(digest.decode('hex')).strip())
+def _boto3_md5sum(digest):
+    """Return the b64 that boto3 expects"""
+    return digest.decode('hex').encode('base64').strip()
 
 
-def _compare_db_with_s3(distribution, key):
-    """Compare a Distribution with an S3 key
+def _compare_db_with_s3(distribution, s3_obj):
+    """Compare a Distribution with an S3 obj
     Returns:
         < 0: S3 Newer
         0:   equal
         > 0: Distribution newer
     """
-    s3_md5 = key.etag.strip('"')
-    if (s3_md5 == distribution.md5_digest):
+    s3_md5 = s3_obj.e_tag.strip('"')
+    if s3_md5 == distribution.md5_digest:
         return 0
 
-    if not key.metadata:
-        bucket = _bucket()
-        key = bucket.get_key(key.name)
-
-    created = dateutil.parser.parse(key.get_metadata('uploaded'))
+    created = dateutil.parser.parse(
+        urllib.unquote(s3_obj.metadata['uploaded']))
     return cmp(distribution.created, created)
 
 
@@ -175,24 +185,19 @@ def _bucket():
     """Return our S3 bucket"""
     global _cached_bucket
     if _cached_bucket is None:
-        bucket_name = getattr(settings, 'PYPI_SYNC_BUCKET')
+        bucket_name = settings.PYPI_SYNC_BUCKET
         if not bucket_name:
             raise Exception("Syncing is disabled")
 
-        conn_data = {
-            'aws_access_key_id': getattr(settings, 'AWS_ACCESS_KEY'),
-            'aws_secret_access_key': getattr(settings, 'AWS_SECRET_KEY'),
-        }
-
-        if '.' in bucket_name:
-            conn_data['calling_format'] = (boto.s3.connection.
-                                           OrdinaryCallingFormat())
-
-        conn = boto.s3.connection.S3Connection(**conn_data)
-        try:
-            _cached_bucket = conn.get_bucket(bucket_name)
-        except boto.exception.S3ResponseError:
-            _cached_bucket = conn.create_bucket(bucket_name)
+        s3 = boto3.resource(
+            's3',
+            aws_access_key_id=settings.AWS_ACCESS_KEY,
+            aws_secret_access_key=settings.AWS_SECRET_KEY,
+            region_name=settings.AWS_REGION_NAME,
+        )
+        _cached_bucket = s3.Bucket(bucket_name)
+        if not _cached_bucket.creation_date:
+            _cached_bucket = s3.create_bucket(Bucket=bucket_name)
     return _cached_bucket
 
 
