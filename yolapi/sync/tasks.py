649ebdc47ce0951a5e401e1d64db6b774a8773aa
kosmos342 <kosmos342@gmail.com>
2024-09-04 13:59:54 +0300

    Isort

--- yolapi/sync/tasks.py
+++ yolapi/sync/tasks.py
@@ -8,8 +8,8 @@ from botocore.exceptions import ClientError
 from django.conf import settings
 from django.core.files import File
 
-from pypi.models import Distribution, Package
 from main import local_celery_app
+from pypi.models import Distribution, Package
 
 log = logging.getLogger(__name__)
 allow_replacement = getattr(settings, 'PYPI_ALLOW_REPLACEMENT', True)
