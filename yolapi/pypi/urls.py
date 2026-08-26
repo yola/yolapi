241ec9d1d5c558ac42ffeee6fd380c3d44e65045
Volodymyr Avramuk <vavramuk@gmail.com>
2026-08-26 19:46:21 +0300

    Fix build for 3.8

--- yolapi/pypi/urls.py
+++ yolapi/pypi/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
         pypi_views.release, name='release'),
     re_path(r'^(?P<package>[\w_.\-]+)/(?P<version>[\w_.\-+]+)'
         r'/(?P<filetype>[\w_]+)/$', pypi_views.delete, name='delete'),
-    re_path(r'^(?P<package>[\w_.\-]+)/(?P<version>[\w_.\-+]+)'
+    re_path(
+        r'^(?P<package>[\w_.\-]+)/(?P<version>[\w_.\-+]+)'
         r'/(?P<filetype>[\w_]+)/(?P<pyversion>[\w.]+)/$',
-        pypi_views.delete, name='delete'),
+        pypi_views.delete, name='delete'
+    ),
 ]
